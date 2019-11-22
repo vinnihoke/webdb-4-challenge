19188383f523710a21ff50a5c35b6f9de621a2e2
Vinni Hoke <vinni.hoke@gmail.com>
2019-11-21 19:36:38 -0800

    Knex config prepped

---
+++ routers/ingredients/ingredient-router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const Ingredients = require('./ingredient-model.js');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const item = await Ingredients.find();
+        res.status(200).json({ message: "Successfully ...", item });    
+    } catch (e) {
+        res.status(500).json({ message: "This is awkward...", error: e.message });
+    }
+})
--- routers/recipes/recipe-router.js
+++ routers/recipes/recipe-router.js
@@ -10,6 +10,5 @@ router.get('/', async (req, res) => {
         res.status(200).json({ message: "Successfully ...", item });    
     } catch (e) {
         res.status(500).json({ message: "This is awkward...", error: e.message });
-
     }
 })
