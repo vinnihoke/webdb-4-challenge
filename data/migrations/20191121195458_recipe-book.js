e201fecb3a6603ad9c63e4557331a3843004c46e
Vinni Hoke <vinni.hoke@gmail.com>
2019-11-21 22:12:12 -0800

    Added migration data and seed files, data fill next

--- data/migrations/20191121195458_recipe-book.js
+++ data/migrations/20191121195458_recipe-book.js
@@ -1,8 +1,53 @@
 
 exports.up = function(knex) {
-  
+  return (
+	  knex.schema
+	  .createTable('recipes', table => {
+		  table.increments();
+		  table.string('recipe_name', 255).notNullable().unique();
+	  })
+	  .createTable('steps', table => {
+		  table.increments();
+		  table.increments('step_number');
+		  table.string('instructions').notNullable()
+		  table.integer('recipe_id')
+			  .unsigned()
+			  .notNullable()
+			  .references('recipes.id')
+			  .onDelete('CASCADE')
+			  .onUpdate('CASCADE')
+	  })
+	  .createTable('ingredients', table => {
+		  table.increments();
+		  table.string('ingredient_name', 255).notNullable().unique();
+	  })
+	  .createTable('recipe_ingredients', table => {
+		  table.primary(["recipe_id", "ingredient_id"])
+		  table.integer('recipe_id')
+			  .unsigned()
+			  .notNullable()
+			  .references('recipes.id')
+			  .onDelete('CASCADE')
+			  .onUpdate('CASCADE')
+		  table.integer('ingredient_id')
+			  .unsigned()
+			  .notNullable()
+			  .references('ingredients.id')
+			  .onDelete('CASCADE')
+			  .onUpdate('CASCADE')
+			table.float('quantity', 3).notNullable()
+			table.string('measurement', 255).notNullable()	
+	  })
+
+  )
 };
 
 exports.down = function(knex) {
-  
+  return(
+	  knex.schema
+	  	.dropTableIfExists('recipe_ingredients')
+	  	.dropTableIfExists('ingredients')
+	  	.dropTableIfExists('steps')
+	  	.dropTableIfExists('recipes')
+  )
 };
