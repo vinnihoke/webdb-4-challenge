47015fb4d96e72d913dd87fbde988dd815070085
Vinni Hoke <vinni.hoke@gmail.com>
2019-11-21 22:33:50 -0800

    Migration and Seeds Successful (Requires Testing)

--- data/seeds/04-recipe_ingredients.js
+++ data/seeds/04-recipe_ingredients.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex) {
-	return knex('steps').insert([
+	return knex('recipe_ingredients').insert([
 		{ recipe_id: 1, ingredient_id: 1, quantity: 1, measurement: "Unit" },
 		{ recipe_id: 1, ingredient_id: 2, quantity: 1, measurement: "Unit" },
 		{ recipe_id: 1, ingredient_id: 3, quantity: 1, measurement: "Unit/Halved" },
